2e39a8df6986af69340edff6889f89f9007a6c6c
Kuniaki Igarashi <igaiga@gmail.com>
2020-03-30 08:19:16 +0900

    disable user auth

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,28 +1,28 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    # action_cable/connection/identification.rb#identified_by
-    # 引数で渡されたシンボルをattr_accessorする
-    identified_by :current_user
+    # # action_cable/connection/identification.rb#identified_by
+    # # 引数で渡されたシンボルをattr_accessorする
+    # identified_by :current_user
 
-    def connect
-      # p "********** #{self.current_user.inspect}"
-      # p "********** #{self.method(:current_user).source_location}"
-      # identified_byで定義されたattr_accessordの代入メソッド
-      self.current_user = find_verified_user
-    end
+    # def connect
+    #   # p "********** #{self.current_user.inspect}"
+    #   # p "********** #{self.method(:current_user).source_location}"
+    #   # identified_byで定義されたattr_accessordの代入メソッド
+    #   self.current_user = find_verified_user
+    # end
 
-    private
+    # private
 
-    def find_verified_user
-      # p "******find_verified_user"
-      # 別の場所で cookies.signed[:user_id]が入っている前提
-      if verified_user = User.find_by(id: cookies.signed[:user_id])
-        verified_user
-      else
-        # p "reject!!!!!!!!!!!!"
-        # ActionCableの接続を切るメソッド
-        reject_unauthorized_connection
-      end
-    end
+    # def find_verified_user
+    #   # p "******find_verified_user"
+    #   # 別の場所で cookies.signed[:user_id]が入っている前提
+    #   if verified_user = User.find_by(id: cookies.signed[:user_id])
+    #     verified_user
+    #   else
+    #     # p "reject!!!!!!!!!!!!"
+    #     # ActionCableの接続を切るメソッド
+    #     reject_unauthorized_connection
+    #   end
+    # end
   end
 end
