8373752b4af98a958d67ce4fadf33d6e23bfc804
Kuniaki Igarashi <igaiga@gmail.com>
2020-04-03 08:19:53 +0900

    Writing test

--- test/channels/room_channel_test.rb
+++ test/channels/room_channel_test.rb
@@ -3,7 +3,10 @@ require 'test_helper'
 class RoomChannelTest < ActionCable::Channel::TestCase
   test 'subscribes' do
     subscribe # 購読作成をシミュレート
-    assert subscription.confirmed? # subscriptionでChannelオブジェクトを取得できる
-    assert_has_stream 'room_channel' # RoomChannel#subscribedメソッドの stream_from で指定した識別文字列(pubsub queue名)
+    # p "*****subscription.class"
+    # p method(:subscription).source_location
+    # p subscription.class
+    assert subscription.confirmed? # subscriptionでRoomChannelオブジェクトを取得できる
+    assert_has_stream 'room_channelfoo' # RoomChannel#subscribedメソッドの stream_from で指定した識別文字列(pubsub queue名)
   end
 end
