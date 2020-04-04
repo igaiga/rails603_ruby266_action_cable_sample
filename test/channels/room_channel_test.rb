d98f089d00eb33ffc371e0b54503dbe900bdde85
Kuniaki Igarashi <igaiga@gmail.com>
2020-04-04 10:56:24 +0900

    Writing RoomChannel test

--- test/channels/room_channel_test.rb
+++ test/channels/room_channel_test.rb
@@ -1,12 +1,21 @@
 require 'test_helper'
 
 class RoomChannelTest < ActionCable::Channel::TestCase
-  test 'subscribes' do
+  test 'subscribe' do
     subscribe # 購読作成をシミュレート
-    # p "*****subscription.class"
-    # p method(:subscription).source_location
-    # p subscription.class
     assert subscription.confirmed? # subscriptionでRoomChannelオブジェクトを取得できる
-    assert_has_stream 'room_channelfoo' # RoomChannel#subscribedメソッドの stream_from で指定した識別文字列(pubsub queue名)
+    assert_has_stream 'room_channel' # RoomChannel#subscribedメソッドの stream_from で指定したストリーム名
+  end
+
+  test 'broadcast' do
+    subscribe
+    text = "hello"
+    broadcast_text = ApplicationController.render(
+      partial: 'messages/message',
+      locals: { message: Message.new(content: text) }
+    )
+    assert_broadcast_on('room_channel', message: broadcast_text) do
+      perform :speak, message: text
+    end
   end
 end
