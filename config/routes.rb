f5eb92d4e61aa05fe2d15f9e9aa05a0e9c08843e
Kuniaki Igarashi <igaiga@gmail.com>
2020-04-07 08:59:26 +0900

    Remove to: from root routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users
   get 'rooms/show'
-  root to: 'rooms#show'
+  root 'rooms#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
